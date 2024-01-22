a2f17e7d2c1e97a60dad027d33555c922f6ed891
Bogdan Eremin <55001168+kixygod@users.noreply.github.com>
2024-01-23 02:30:07 +0700

    netlify deploy

--- react-practice/src/index.js
+++ react-practice/src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './styles/Main.scss';
-import Users from './pages/Users'
-import Main from './pages/Main'
-import NoPage from './pages/NoPage'
-import UserInfo from './pages/UserInfo';
-
-
+import "./styles/Main.scss";
+import Users from "./pages/Users";
+import Main from "./pages/Main";
+import NoPage from "./pages/NoPage";
+import UserInfo from "./pages/UserInfo";
 
 export default function App() {
   return (
@@ -17,10 +15,9 @@ export default function App() {
         <Route path="users" element={<Users />} />
         <Route path="/users/:id" element={<UserInfo />} />
         <Route path="*" element={<NoPage />} />
-
       </Routes>
     </BrowserRouter>
   );
 }
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
